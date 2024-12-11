6e0fb1ff91b819fb0da488a7d33ad40d68f40bcd
Ramirtsson <rammirtson@gmail.com>
2024-12-11 08:39:36 -0600

    implementacion/ libreria swagger

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,7 +1,13 @@
 <?php
 
 namespace App\Http\Controllers;
-
+/**
+ * @OA\Info(
+ *      version="1.0",
+ *      title="Tickets",
+ *      description="API Documentation for Tickets Project"
+ * )
+ */
 abstract class Controller
 {
     //
